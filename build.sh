85aef8b812919c0b7ae597cd259f3bc8545ef91e
Chris Simpkins <git.simpkins@gmail.com>
2015-12-24 15:05:47 -0500

    added new build script

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+javac src/at/fruel/font/ArgumentParser.java -d build
+javac src/at/fruel/font/JFontChooser.java -d build
+javac src/at/fruel/font/Main.java -d build
+javac src/at/fruel/font/RenderWindow.java -d build
+
+
